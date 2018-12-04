64b66250930db7d4b496b8e6158446a01799a83a
omerduzyol <omer@duzyol.net>
2018-12-04 21:38:32 +0000

    `allowShorthand` defaulted to `true`

--- index.js
+++ index.js
@@ -8,8 +8,11 @@ import pluginAtIf from "postcss-conditionals";
 import extendInternal from "./lib/extend";
 import { atRules, rules } from "./lib/plugin";
 
+/**
+ * opts: { allowShorthands: ?boolean }
+ */
 module.exports = postcss.plugin("postcss-react-bender", function(opts) {
-  opts = opts || {};
+  const pluginOptions = { allowShorthand:true, ...opts };
 
   return postcss([
     pluginImports,
@@ -21,8 +24,8 @@ module.exports = postcss.plugin("postcss-react-bender", function(opts) {
   ]).use((root, result) => {
     let tree = {};
 
-    root.walkAtRules(rule => atRules(opts, { root, rule, tree }));
-    root.walkRules(rule => rules(opts, { root, rule, tree }));
+    root.walkAtRules(rule => atRules(pluginOptions, { root, rule, tree }));
+    root.walkRules(rule => rules(pluginOptions, { root, rule, tree }));
 
     result.bender = tree;
   });
