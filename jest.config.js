b8c406f2446f2086a632e07dac9c619aaff8bd64
omerduzyol <omer@duzyol.net>
2018-12-04 22:17:13 +0000

    Jest config extracted.

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "testEnvironment": "node",
+  "testMatch": ["**/test.spec.js?(x)"],
+  "coverageReporters": ['html', 'text', 'text-summary'],
+  "roots": [
+    "<rootDir>/tests/"
+  ]
+}
